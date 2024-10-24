44c2817bdae407210ac800bee7d0fd25540e219b
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-10-24 23:56:38 +0500

    DOM

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const input = document.querySelector("input");
-const text = document.querySelector("h3");
-input.addEventListener("input", () => {
-  text.style.fontSize = input.value + "px";
-});
+// 1 Misol
+// const content = document.querySelector("#content");
+// const paragraph = document.createElement("p");
+// paragraph.textContent = "I am Bekhzod Rasulov";
+// content.appendChild(paragraph);
+
+// 2 Misol
+// const changeText = document.querySelector("#changeText");
+// const text = document.querySelector("#text");
+// changeText.addEventListener("click", () => {
+//   text.textContent = "This is a new text!";
+// });
+
+//  3 Misol
+// const button1 = document.querySelector("#button1");
+// const button2 = document.querySelector("#button2");
+// const button3 = document.querySelector("#button3");
+// const imageId = document.querySelector("#imageId");
+// button1.addEventListener("click", () => {
+//   imageId.src = `https://picsum.photos/id/${Math.ceil(
+//     Math.random() * 300
+//   )}/1000`;
+// });
+// button2.addEventListener("click", () => {
+//   imageId.src = `https://picsum.photos/id/${Math.ceil(
+//     Math.random() * 300
+//   )}/1000`;
+// });
+// button3.addEventListener("click", () => {
+//   imageId.src = `https://picsum.photos/id/${Math.ceil(
+//     Math.random() * 300
+//   )}/1000`;
+// });
